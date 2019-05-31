53116ecd65b95014b5f26403cf695a0e15c850b0
miloEgon <2467233254@qq.com>
2019-05-31 20:08:25 +0800

    CC

---
+++ src/main/java/com/cheham/dean/app/ComsumerApp.java
@@ -0,0 +1,19 @@
+package com.cheham.dean.app;
+
+import com.cheham.dean.service.Calculater;
+import com.cheham.dean.service.CalculatorRemoteImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComsumerApp {
+
+    private static Logger logger = LoggerFactory.getLogger(ComsumerApp.class);
+
+    public static void main(String[] args) {
+        Calculater calculater = new CalculatorRemoteImpl();
+        int result1 = calculater.add(25, 45);
+        logger.info("结果是："+result1);
+        int result2 = calculater.subtract(100, 1);
+        logger.info("结果是："+result2);
+    }
+}
